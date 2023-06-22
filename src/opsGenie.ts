227cf004244d23d77cda227b949a49a196b340ed
Jack Nugent <jack.nugent@washpost.com>
2023-06-22 09:51:23 -0400

    Add alert log

--- src/opsGenie.ts
+++ src/opsGenie.ts
@@ -87,6 +87,7 @@ async function isAlertCurrentlyOpen(alias: keyof typeof CartaAlerts) {
 }
 
 export async function closeOpenAlert(alias: keyof typeof CartaAlerts) {
+    console.log(`Successful check of ${alias}. Closing alert if it is open.`);
     const isOpen = await isAlertCurrentlyOpen(alias);
     if (!isOpen) {
         return;
