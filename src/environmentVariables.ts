b6cc7582a6f34404986a15b1950d404dab740ea8
Jack Nugent <jack.nugent@washpost.com>
2023-06-28 14:41:59 -0400

    Simplify environment variables

--- src/environmentVariables.ts
+++ src/environmentVariables.ts
@@ -1,30 +1,32 @@
-enum EnvVars {
-    NONPERSONALIZED_CAMPAIGN_ID = "NONPERSONALIZED_CAMPAIGN_ID",
-    PERSONALIZED_CAMPAIGN_ID = "PERSONALIZED_CAMPAIGN_ID",
-    TRANSACTIONAL_CAMPAIGN_ID = "TRANSACTIONAL_CAMPAIGN_ID",
-    LIST_MANAGEMENT_SEND_ALERT = "LIST_MANAGEMENT_SEND_ALERT",
-    ALERT_CAMPAIGN_NAME = "ALERT_CAMPAIGN_NAME",
-    ALERT_EMAIL_LIST = "ALERT_EMAIL_LIST",
-    OPS_GENIE_ENV = "OPS_GENIE_ENV",
-    MONGODB_NAME = "MONGODB_NAME",
-    MONGODB_URI = "MONGODB_URI",
-    CARTA_UI_BASE_URL = "CARTA_UI_BASE_URL",
-    NONPERSONALIZED_SENDER_URL = "NONPERSONALIZED_SENDER_URL",
-    STAGE = "STAGE",
-    IS_LOCAL = "IS_LOCAL",
-    METRICS_EVENTS_COUNT_ALERT_THRESHHOLD = "METRICS_EVENTS_COUNT_ALERT_THRESHHOLD",
-    FILE_DOWNLOAD_PROCESSING_THRESHHOLD_MINUTES = "FILE_DOWNLOAD_PROCESSING_THRESHHOLD_MINUTES",
-    SENDS_PER_ALLOWED_TIME_SEGMENT = "SENDS_PER_ALLOWED_TIME_SEGMENT",
-    MINUTES_PER_ALLOWED_TIME_SEGMENT = "MINUTES_PER_ALLOWED_TIME_SEGMENT",
-    SUCCESSFUL_SEND_COMPLETION_PERCENTAGE = "SUCCESSFUL_SEND_COMPLETION_PERCENTAGE"
-}
+const EnvVars = [
+    "NONPERSONALIZED_CAMPAIGN_ID",
+    "PERSONALIZED_CAMPAIGN_ID",
+    "TRANSACTIONAL_CAMPAIGN_ID",
+    "LIST_MANAGEMENT_SEND_ALERT",
+    "ALERT_CAMPAIGN_NAME",
+    "ALERT_EMAIL_LIST",
+    "OPS_GENIE_ENV",
+    "MONGODB_NAME",
+    "MONGODB_URI",
+    "CARTA_UI_BASE_URL",
+    "NONPERSONALIZED_SENDER_URL",
+    "STAGE",
+    "IS_LOCAL",
+    "METRICS_EVENTS_COUNT_ALERT_THRESHHOLD",
+    "FILE_DOWNLOAD_PROCESSING_THRESHHOLD_MINUTES",
+    "SENDS_PER_ALLOWED_TIME_SEGMENT",
+    "MINUTES_PER_ALLOWED_TIME_SEGMENT",
+    "SUCCESSFUL_SEND_COMPLETION_PERCENTAGE"
+] as const;
 
-type EnvVarValues = { [K in EnvVars]: string };
+type EnvVar = (typeof EnvVars)[number];
+
+type EnvVarValues = { [K in EnvVar]: string };
 
 const getAndCheckRequiredEnvVars = () => {
     const cache: Partial<EnvVarValues> = {};
 
-    for (const varName of Object.values(EnvVars)) {
+    for (const varName of EnvVars) {
         const value = process.env[varName];
 
         // Checks both for missing variables and empty/null strings
@@ -33,7 +35,7 @@ const getAndCheckRequiredEnvVars = () => {
                 `Missing or empty ${varName} in environment variables`
             );
         } else {
-            cache[varName as EnvVars] = value;
+            cache[varName] = value;
         }
     }
 
