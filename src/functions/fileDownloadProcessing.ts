03c311e847df9f2e0b2e16ece67556e548a259d4
Jack Nugent <jack.nugent@washpost.com>
2023-07-11 13:24:47 -0400

    Improve error message

--- src/functions/fileDownloadProcessing.ts
+++ src/functions/fileDownloadProcessing.ts
@@ -6,6 +6,19 @@ import { envVars } from "../environmentVariables";
 import middy from "@middy/core";
 import { errorHandlerMiddleware } from "../errorMiddleware";
 
+const generateProcessingMessage = (listNames: string[]) => {
+    const message = `${
+        listNames.length
+    } file(s) currently processing: ${listNames.join(", ")}.
+0. Sometimes a false postive, like when Marcy was running a test on Dynamic Lists. Can reach out to her to confirm.
+1. Go to CartaWashPostProd mongo db collection and query file_download_details collection
+   a. Verify at least one list is processing (status = processing), and its processed_count increases when you refresh
+2. If none are processing, more investigation is required (is the Java process running?)
+3. Verify your list is either status processing or submitted. If submitted, it is in the queue, and you should just keep an eye out to make sure the lists get queued eventually. Do not kill a download until someone makes a very strong case that their backed-up list download is urgent. May want to check in with user who triggered the list to let them know it is taking a while (found under user key in collection)`;
+
+    return message;
+};
+
 export const baseCheckFileDownloadProcessing = async () => {
     const { db, client } = await getMongoDatabase();
     const fileDownloadCollection = db.collection("file_download_details");
@@ -44,9 +57,7 @@ export const baseCheckFileDownloadProcessing = async () => {
 
     await createAlert(
         CartaAlerts.File_Download_Processing_Delay,
-        `${
-            submittedDownloadList.length
-        } file(s) currently processing: ${listNames.join(", ")}`
+        generateProcessingMessage(listNames)
     );
 
     return {
