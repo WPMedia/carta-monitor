cf07f9c4dbfc83ff31e8890d4dc6d676d236c507
Jack Nugent <jack.nugent@washpost.com>
2023-07-12 12:09:22 -0400

    Remove log

--- src/mongo.ts
+++ src/mongo.ts
@@ -14,7 +14,6 @@ export const getMongoDatabase = async (): Promise<{
         "{0}",
         mongoConnectionStringPassword
     );
-    console.log(`connection string debug: ${mongoUri}`);
     const client = new MongoClient(mongoUri);
 
     try {
